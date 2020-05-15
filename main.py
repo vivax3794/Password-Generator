0bc735e8939d06c3d506a347ef513784341d2908
ManavRaja <manavrv2005@gmail.com>
2020-05-15 13:15:13 -0500

    Added function to make sure input is an integer.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from password_generator import PasswordGenerator
 
 
+def input_int(question):
+    num = input(question)
+    while num.isnumeric() == False:
+        print("That's not an integer. You can only input an integer! Please try again.")
+        num = input(question)
+    return num
+
+
 pwd = PasswordGenerator()
 
-pwd_min_length = input("What do you want the password's minimum length to be?")
-pwd_max_length = input("What do you want the password's maximum length to be?")
-pwd_min_upper_case = input("How many characters at the minimum should be uppercase?")
-pwd_min_lower_case = input("How many characters at the minimum should be lowercase?")
-pwd_min_num = input("How many numbers at the minimum should the passowrd contain?")
-pwd_min_spec_char = input("How many characters at the minimum should be special?")
+pwd_min_length = input_int("What do you want the password's minimum length to be?  ")
+pwd_max_length = input_int("What do you want the password's maximum length to be?  ")
+pwd_min_upper_case = input_int("How many characters at the minimum should be uppercase?  ")
+pwd_min_lower_case = input_int("How many characters at the minimum should be lowercase?  ")
+pwd_min_num = input_int("How many numbers at the minimum should the passowrd contain?  ")
+pwd_min_spec_char = input_int("How many characters at the minimum should be special?  ")
 
 pwd.minlen = int(pwd_min_length)
 pwd.maxlen = int(pwd_max_length)
